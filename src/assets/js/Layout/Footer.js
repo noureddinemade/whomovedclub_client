4dfbc8a89ff3762087c9366c3f9834c12c7c1717
Noureddine Azhar <nrdnazhr@gmail.com>
2020-09-14 17:06:43 +1000

    Fixed favicon

--- src/assets/js/Layout/Footer.js
+++ src/assets/js/Layout/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer>
             <ul>
                 <li>
-                    View on <a href="https://github.com/noureddinemade/whomovedclub" target="_blank" rel="noopener noreferrer">Github</a>
+                    View on <a href="https://github.com/noureddinemade/whomovedclub_client" target="_blank" rel="noopener noreferrer">Github</a>
                 </li>
                 <li>&copy; <a href="https://noureddine.biz" target="_blank" rel="noopener noreferrer">Noureddine</a> made this.</li>
             </ul>
